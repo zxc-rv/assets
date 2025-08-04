d6b89403f8715b5039c945166e5dfa2a638658d9
zxc-rv <the.revolution@icloud.com>
2025-08-04 23:22:21 +0300

    Update nvim-setup.sh

--- nvim-setup.sh
+++ nvim-setup.sh
@@ -9,47 +9,43 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
-echo -e "${GREEN}Ставим fuse3...${NC}"
+echo -e "\n${GREEN}Установка fuse3...${NC}"
 apt update -qq && apt install -y fuse3 -qq > /dev/null 2>&1 || {
   echo -e "${RED}Не смог поставить fuse3, чекни логи!${NC}" >&2
   exit 1
 }
 
-echo -e "${GREEN}Качаем Neovim AppImage...${NC}"
+echo -e "\n${GREEN}Установка Neovim...${NC}"
 wget -q -O nvim-linux-x86_64.appimage https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.appimage || {
-  echo -e "${RED}Не смог скачать Neovim, проверь инет!${NC}" >&2
+  echo -e "\n${RED}Не смог скачать Neovim, проверь инет!${NC}" >&2
   exit 1
 }
-
-echo -e "${GREEN}Делаем AppImage исполняемым...${NC}"
 chmod u+x nvim-linux-x86_64.appimage || {
-  echo -e "${RED}Не смог дать права, что-то не так!${NC}" >&2
+  echo -e "\n${RED}Не смог дать права, что-то не так!${NC}" >&2
   exit 1
 }
-
-echo -e "${GREEN}Кидаем Neovim в /usr/local/bin...${NC}"
 mv nvim-linux-x86_64.appimage /usr/local/bin/nvim || {
-  echo -e "${RED}Не смог переместить, проверь права!${NC}" >&2
+  echo -e "\n${RED}Не смог переместить, проверь права!${NC}" >&2
   exit 1
 }
 
 
-echo -e "${GREEN}Настраиваем init.lua...${NC}"
+echo -e "\n${GREEN}Настройка init.lua...${NC}"
 mkdir -p ~/.config/nvim
 
 cat << 'EOF' > ~/.config/nvim/init.lua
 vim.g.clipboard = 'osc52'
+vim.g.mapleader = " "
 vim.o.number = true
+vim.o.swapfile = false
+vim.o.winborder = "rounded"
 vim.keymap.set('v', '<C-C>', '"+y', { noremap = true })
+vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
-vim.o.swapfile = false
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 vim.cmd(":hi statusline guibg=NONE guifg=#ffffff")
-vim.g.mapleader = " "
-vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
-vim.o.winborder = "rounded"
 vim.pack.add{
   { src = 'https://github.com/echasnovski/mini.pick' },
 }
@@ -58,4 +54,4 @@ require "mini.pick".setup()
 
 EOF
 
-echo -e "${GREEN}Neovim успешно установлен.${NC}"
+echo -e "\n${GREEN}Neovim успешно установлен.${NC}"
