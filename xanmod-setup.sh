a3fcb3d27538c1426b288a00772b316feca4031e
zxc-rv <the.revolution@icloud.com>
2025-08-07 23:21:31 +0300

    Update xanmod-setup.sh

--- xanmod-setup.sh
+++ xanmod-setup.sh
@@ -87,7 +87,58 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-(sleep 0.5 && grep -Fx "net.core.default_qdisc=fq" /etc/sysctl.conf >/dev/null || echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf; grep -Fx "net.ipv4.tcp_congestion_control=bbr" /etc/sysctl.conf >/dev/null || echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf; sysctl -p >/dev/null 2>&1) & spinner $! "Применение настроек sysctl..."
+(sleep 0.5 && \
+sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf && \
+sed -i '/net.core.rmem_max/d' /etc/sysctl.conf && \
+sed -i '/net.core.wmem_max/d' /etc/sysctl.conf && \
+sed -i '/net.core.wmem_default/d' /etc/sysctl.conf && \
+sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf && \
+sed -i '/net.core.somaxconn/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_syncookies/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_tw_reuse/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_fin_timeout/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_keepalive_time/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_keepalive_probes/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_keepalive_intvl/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_max_syn_backlog/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_max_tw_buckets/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_fastopen/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_timestamps/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_mem/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.udp_mem/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_rmem/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_wmem/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_mtu_probing/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.tcp_slow_start_after_idle/d' /etc/sysctl.conf && \
+sed -i '/fs.inotify.max_user_instances/d' /etc/sysctl.conf && \
+sed -i '/net.ipv4.ip_local_port_range/d' /etc/sysctl.conf && \
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf && \
+echo "net.core.rmem_max = 67108864" >> /etc/sysctl.conf && \
+echo "net.core.wmem_max = 67108864" >> /etc/sysctl.conf && \
+echo "net.core.wmem_default = 2097152" >> /etc/sysctl.conf && \
+echo "net.core.netdev_max_backlog = 10240" >> /etc/sysctl.conf && \
+echo "net.core.somaxconn = 8192" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_syncookies = 1" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_tw_reuse = 1" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_fin_timeout = 30" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_keepalive_time = 1200" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_keepalive_probes = 5" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_keepalive_intvl = 30" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_max_syn_backlog = 10240" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_max_tw_buckets = 5000" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_timestamps = 1" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_mem = 25600 51200 102400" >> /etc/sysctl.conf && \
+echo "net.ipv4.udp_mem = 25600 51200 102400" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_rmem = 16384 262144 8388608" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_wmem = 32768 524288 16777216" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_mtu_probing = 1" >> /etc/sysctl.conf && \
+echo "net.ipv4.tcp_slow_start_after_idle=0" >> /etc/sysctl.conf && \
+echo "fs.inotify.max_user_instances = 8192" >> /etc/sysctl.conf && \
+echo "net.ipv4.ip_local_port_range = 1024 45000" >> /etc/sysctl.conf && \
+sysctl -p >/dev/null 2>&1) & spinner $! "Оптимизация настроек sysctl..."
 if [ $? -ne 0 ]; then
     echo -e "${RED}Ошибка настройки и применения sysctl${NC}" >&2
     exit 1
